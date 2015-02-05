0cf3ce9e1ae5afd403ddbb85a46d0be402069420
Taye Adeyemi <dev@taye.me>
2015-02-05 21:26:49 +0000

    Update bundled files

--- META-INF/resources/webjars/ethereumjs-lib/poc-8-wip/index.js
+++
@@ -1,8 +0,0 @@
-/* jshint node: true */
-module.exports = {
-    'Ethereum': require('ethereumjs-lib'),
-    'buffer': require('buffer'),
-    'bignum': require('ethereumjs-lib/browser/bignum'),
-    'ecdsaOps': require('ethereumjs-lib/browser/ecdsaOps'),
-    'rlp': require('rlp')
-};
