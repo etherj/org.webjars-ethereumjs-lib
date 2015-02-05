d1df73bcf4f285917c3044925e5e07f275955aa6
Taye Adeyemi <dev@taye.me>
2015-02-05 22:37:27 +0000

    Use uglify-js instead of minify for minification

--- src/build.js
+++ src/build.js
@@ -3,26 +3,23 @@
 'use strict';
 
 var browserify = require('browserify'),
-    minify = require('minify'),
+    uglify = require('uglify-js'),
     fs = require('fs'),
     targetDir = '../META-INF/resources/webjars/ethereumjs-lib/poc-8-wip',
-    //targetDir = '.',
-    targetDevFile = targetDir + '/ethereumjs.js';
+    devDest = targetDir + '/ethereumjs.js';
 
 var b = browserify({ standalone: 'ethereum' }),
-    devWS = fs.createWriteStream(targetDevFile),
-    minWS = fs.createWriteStream(targetDir + '/ethereumjs.min.js');
+    devWrite = fs.createWriteStream(devDest);
 
-b.add('ethereumjs-lib/lib/transaction.js')
-    .bundle()
-    .pipe(devWS);
+devWrite.on('close', function () {
+var minDest = targetDir + '/ethereumjs.min.js',
+    minWrite = fs.createWriteStream(minDest),
+    uglified = uglify.minify(devDest);
 
-minify(targetDevFile, 'stream', function (error, stream) {
-    if (error) {
-        console.error(error.message);
-    }
-    else {
-        stream.pipe(minWS);
-    }
+    minWrite.write(uglified.code);
 });
+
+b.add('./index')
+    .bundle()
+    .pipe(devWrite);
 
